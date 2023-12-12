5cbc29499b4d5f0340cf23dba9e90f6bcb44e1c0
Niko Junnila <niko.junnila@goodlife.technology>
2023-12-12 16:46:36 +0200

    Add missing ITableStorageService

--- Aire.Sdk.TableStorage/src/ITableStorageService.cs
+++ Aire.Sdk.TableStorage/src/ITableStorageService.cs
@@ -1,6 +1,27 @@
-﻿namespace Aire.Sdk.TableStorage;
+﻿using System.Linq.Expressions;
+using Azure;
+using Azure.Data.Tables;
 
-public class Class1
+namespace Aire.Sdk.TableStorage
 {
+    public interface ITableStorageService 
+    {
+        Task<T?> RetrieveAsync<T>(string key)
+            where T: class, ITableEntity, new();
+            
+        Task<T?> RetrieveAsync<T>(string partitionKey, string rowKey) 
+            where T: class, ITableEntity, new();
 
+        Task<bool> UpsertAsync<T>(T entity)
+            where T: class, ITableEntity, new();
+            
+        Task<bool> DeleteAsync<T>(T entity)
+            where T: class, ITableEntity, new();
+
+        Task<bool> DeleteAsync<T>(string partitionKey, string rowKey)
+            where T: class, ITableEntity, new();
+
+        Task<AsyncPageable<T>> QueryAsync<T>(Expression<Func<T, bool>> expression)
+            where T: class, ITableEntity, new();
+    }
 }
