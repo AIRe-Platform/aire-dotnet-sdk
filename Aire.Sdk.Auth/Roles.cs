26b6d53bb4ea4e154b2a9bc3dfce475f5795bacd
Niko Junnila <niko.junnila@goodlife.technology>
2024-03-04 16:03:36 +0200

    Add new "researcher" and "research-subject" roles. Add new scopes for managing research subjects. Add scope for AIRe Hub access. Refactor AireScopes constructor.

--- Aire.Sdk.Auth/Roles.cs
+++ Aire.Sdk.Auth/Roles.cs
@@ -5,7 +5,10 @@ namespace Aire.Sdk.Auth.Roles
         public const string User = "user";
         public const string Admin = "admin";
 
-        public static readonly AireRoles All = [ User, Admin ];
+        public const string Researcher = "researcher";
+        public const string ResearchSubject = "research-subject";
+
+        public static readonly AireRoles All = [ User, Admin, Researcher, ResearchSubject ];
 
         public AireRoles() : base() {}
         public AireRoles(params string[] roles) : base(roles) {}
--- Aire.Sdk.Auth/Scopes.cs
+++ Aire.Sdk.Auth/Scopes.cs
@@ -1,3 +1,5 @@
+using Aire.Sdk.Auth.Roles;
+
 namespace Aire.Sdk.Auth.Scopes
 {
     public class AireScopes : List<string>
@@ -5,60 +7,88 @@ namespace Aire.Sdk.Auth.Scopes
         public const string ReadProfile = "profile-read";
         public const string EditProfile = "profile-edit";
         public const string DeleteProfile = "profile-delete";
-        public static readonly AireScopes Profile = [ ReadProfile, EditProfile, DeleteProfile ];
+        public static readonly AireScopes Profile = [ReadProfile, EditProfile, DeleteProfile];
 
         public const string ReadChatHistory = "chat-history-read";
         public const string WriteChatHistory = "chat-history-write";
         public const string DeleteChatHistory = "chat-history-delete";
-        public static readonly AireScopes ChatHistory = [ ReadChatHistory, WriteChatHistory, DeleteChatHistory ];
+        public static readonly AireScopes ChatHistory = [ReadChatHistory, WriteChatHistory, DeleteChatHistory];
 
         public const string ReadQuestionnaire = "questionnaire-read";
         public const string WriteQuestionnaire = "questionnaire-write";
         public const string DeleteQuestionnaire = "questionnaire-delete";
-        public static readonly AireScopes Questionnaire = [ ReadQuestionnaire, WriteQuestionnaire, DeleteQuestionnaire ];
-        public static readonly AireScopes QuestionnaireUser = [ ReadQuestionnaire ];
+        public static readonly AireScopes Questionnaire = [ReadQuestionnaire, WriteQuestionnaire, DeleteQuestionnaire];
 
         public const string ReadDocument = "document-read";
         public const string WriteDocument = "document-write";
         public const string DeleteDocument = "document-delete";
-        public static readonly AireScopes Document = [ ReadDocument, WriteDocument, DeleteDocument ];
-        public static readonly AireScopes DocumentUser = [ ReadDocument ];
+        public static readonly AireScopes Document = [ReadDocument, WriteDocument, DeleteDocument];
 
         public const string ChatCompletion = "chat-completion";
         public const string ChatSummary = "chat-summary";
-        public const string ChatEmbeddings = "chat-embeddings";
         public const string ChatTokenCount = "chat-token-count";
-        public static readonly AireScopes ChatBot = [ ChatCompletion, ChatSummary, ChatTokenCount ];
-        public static readonly AireScopes ChatBotAdmin = [ ChatEmbeddings ];
+        public static readonly AireScopes ChatBot = [ChatCompletion, ChatSummary, ChatTokenCount];
 
+        public const string ReadResearchSubject = "research-subject-read";
+        public const string WriteResearchSubject = "research-subject-write";
+        public const string DeleteResearchSubject = "research-subject-delete";
+        public static readonly AireScopes ResearchSubjects = [ReadResearchSubject, WriteResearchSubject, DeleteResearchSubject];
+
+        public const string AireHub = "aire-hub";
         public const string UnverifiedAccount = "unverified-account";
         public const string EulaRequired = "eula-required";
 
-        public static readonly AireScopes All = new(
-            Profile, 
-            ChatHistory, 
-            ChatBot, 
-            ChatBotAdmin, 
-            Questionnaire, 
-            Document
+        public static readonly AireScopes AdminScopes = new(
+            Profile,
+            ChatHistory,
+            ChatBot,
+            Questionnaire,
+            Document,
+            ResearchSubjects,
+            AireHub
         );
 
         public static readonly AireScopes UserScopes = new(
-            Profile, 
-            ChatHistory, 
-            ChatBot, 
-            QuestionnaireUser, 
-            DocumentUser
+            Profile,
+            ChatHistory,
+            ChatBot,
+            ReadQuestionnaire,
+            ReadDocument
         );
 
-        public static readonly AireScopes AdminScopes = new(All);
+        public static readonly AireScopes ResearcherScopes = new(
+            UserScopes,
+            ResearchSubjects,
+            AireHub
+        );
 
-        public AireScopes() : base() {}
-        public AireScopes(params string[] scopes) : base(scopes) {}
-        public AireScopes(params AireScopes[] scopes) 
+        public static readonly AireScopes ResearchSubjectScopes = new(
+            ReadProfile,
+            EditProfile,
+            ChatBot,
+            ChatHistory,
+            ReadQuestionnaire
+        );
+
+        public static readonly Dictionary<string, AireScopes> DefaultRoleScopes = new() {
+            { AireRoles.User, UserScopes },
+            { AireRoles.Admin, AdminScopes },
+            { AireRoles.Researcher, ResearcherScopes },
+            { AireRoles.ResearchSubject, ResearchSubjectScopes}
+        };
+
+        public AireScopes() : base() { }
+        public AireScopes(params dynamic[] collection) : base()
         {
-            foreach(var list in scopes)
-                AddRange(list);
+            foreach (var item in collection)
+            {
+                if (item is string)
+                    Add(item);
+                else if (item is AireScopes)
+                    AddRange(item);
+                else 
+                    throw new ArgumentException("Unsupported argument");
+            }
         }
     }
 }
