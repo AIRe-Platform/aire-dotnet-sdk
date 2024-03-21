c77fec2c3c4901720ff1fe5183e46e7a151dd163
Niko Junnila <niko.junnila@goodlife.technology>
2024-03-21 11:07:31 +0200

    Add BaseTableEntity to Aire.Sdk.Azure.

---
+++ Aire.Sdk.Azure/src/BaseTableEntity.cs
@@ -0,0 +1,14 @@
+
+using Azure;
+using Azure.Data.Tables;
+
+namespace Aire.Sdk.Azure;
+
+public class BaseTableEntity : ITableEntity
+{
+    public virtual string? PartitionKey { get; set; }
+    public virtual string? RowKey { get; set; }
+    public DateTimeOffset? Timestamp { get; set; }
+    public ETag ETag { get; set; }
+}
+
