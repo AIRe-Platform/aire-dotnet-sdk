3020f09f46272adcc4916c6f82f89ac2e2827831
JuanDGoodlifeTechnology <juan.deltell@goodlife.technology>
2024-04-08 15:01:58 +0300

    Content in the submodule

--- Aire.Sdk.Models/src/Resources/Content.cs
+++ Aire.Sdk.Models/src/Resources/Content.cs
@@ -6,26 +6,37 @@ namespace Aire.Sdk.Models.Resources
     {
         [JsonProperty("id")]
         public Guid? Id { get; set; } = Guid.NewGuid();
+        
         [JsonProperty("name")]
         public string? Name { get; set; }
+        
         [JsonProperty("description")]
         public string? Description { get; set; }
+        
         [JsonProperty("hidden")]
         public bool? Hidden { get; set; }
+        
         [JsonProperty("type")]
         public string? Type { get; set; }
+        
         [JsonProperty("url")]
         public string? Url { get; set; }
+        
         [JsonProperty("views_count")]
         public int? ViewsCount { get; set; }
+        
         [JsonProperty("viewers_rating")]
         public int? ViewersRating { get; set; }
+        
         [JsonProperty("injured_type")]
         public string? InjuredType { get; set; }
+        
         [JsonProperty("age")]
         public string? Age { get; set; }
+        
         [JsonProperty("gender")]
         public string? Gender { get; set; }
+        
         [JsonProperty("modified")]
         public DateTime Modified { get; set; }
 
