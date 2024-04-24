3865a209ff0c6679b9bba00502351244859761fd
Niko Junnila <niko.junnila@goodlife.technology>
2024-04-24 14:34:58 +0300

    Clean up Content model

--- Aire.Sdk.Models/src/Resources/Content.cs
+++ Aire.Sdk.Models/src/Resources/Content.cs
@@ -1,36 +1,42 @@
 using Newtonsoft.Json;
 
-namespace Aire.Sdk.Models.Resources
+namespace Aire.Sdk.Models.Resources;
+
+public class Content
 {
-    public class Content
-    {
-        [JsonProperty("id")]
-        public Guid? Id { get; set; } = Guid.NewGuid();
-        [JsonProperty("name")]
-        public string? Name { get; set; }
-        [JsonProperty("description")]
-        public string? Description { get; set; }
-        [JsonProperty("hidden")]
-        public bool? Hidden { get; set; }
-        [JsonProperty("type")]
-        public string? Type { get; set; }
-        [JsonProperty("url")]
-        public string? Url { get; set; }
-        [JsonProperty("views_count")]
-        public int? ViewsCount { get; set; }
-        [JsonProperty("viewers_rating")]
-        public int? ViewersRating { get; set; }
-        [JsonProperty("injured_type")]
-        public string? InjuredType { get; set; }
-        [JsonProperty("modified")]
-        public DateTime Modified { get; set; }
-        [JsonProperty("keywords")]
-        public string[]? Keywords { get; set; }
-        [JsonProperty("blobName")]
-        public string? BlobName { get; set; }
-        
+    [JsonProperty("id")]
+    public Guid? Id { get; set; } = Guid.NewGuid();
 
+    [JsonProperty("name")]
+    public string? Name { get; set; }
 
-        public Content() { }
-    }
+    [JsonProperty("description")]
+    public string? Description { get; set; }
+
+    [JsonProperty("hidden")]
+    public bool? Hidden { get; set; }
+
+    [JsonProperty("type")]
+    public string? Type { get; set; }
+
+    [JsonProperty("url")]
+    public string? Url { get; set; }
+
+    [JsonProperty("views_count")]
+    public int? ViewsCount { get; set; }
+
+    [JsonProperty("viewers_rating")]
+    public int? ViewersRating { get; set; }
+
+    [JsonProperty("injured_type")]
+    public string? InjuredType { get; set; }
+
+    [JsonProperty("modified")]
+    public DateTime Modified { get; set; }
+
+    [JsonProperty("keywords")]
+    public string[]? Keywords { get; set; }
+
+    [JsonProperty("blobName")]
+    public string? BlobName { get; set; }
 }
