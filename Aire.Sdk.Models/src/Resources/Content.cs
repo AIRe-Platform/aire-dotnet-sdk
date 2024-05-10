812b44d3fbd9a51f5e82605fa8fef2ee1cc7a285
Niko Junnila <niko.junnila@goodlife.technology>
2024-05-10 13:04:50 +0300

    Add enum for content type. Remove unnecessary Content properties.

--- Aire.Sdk.Models/src/Resources/Content.cs
+++ Aire.Sdk.Models/src/Resources/Content.cs
@@ -1,6 +1,36 @@
+using System.Runtime.Serialization;
 using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
 
 namespace Aire.Sdk.Models.Resources;
+
+[JsonConverter(typeof(StringEnumConverter))]
+public enum ContentType
+{
+    [EnumMember(Value = "url")]
+    URL,
+
+    [EnumMember(Value = "image")]
+    Image,
+
+    [EnumMember(Value = "video")]
+    Video,
+
+    [EnumMember(Value = "document")]
+    Document,
+}
+
+public static class ContentTypeExtensions
+{
+    public static bool IsBlobType(this ContentType type)
+    {
+        return type switch
+        {
+            ContentType.Image or ContentType.Video or ContentType.Document => true,
+            _ => false,
+        };
+    }
+}
 
 public class Content
 {
@@ -16,11 +46,11 @@ public class Content
     [JsonProperty("hidden")]
     public bool? Hidden { get; set; }
 
-    [JsonProperty("type")]
-    public string? Type { get; set; }
-
     [JsonProperty("url")]
     public string? Url { get; set; }
+
+    [JsonProperty("type")]
+    public ContentType? Type { get; set; }
 
     [JsonProperty("views_count")]
     public int? ViewsCount { get; set; }
@@ -28,15 +58,9 @@ public class Content
     [JsonProperty("viewers_rating")]
     public int? ViewersRating { get; set; }
 
-    [JsonProperty("injured_type")]
-    public string? InjuredType { get; set; }
-
     [JsonProperty("modified")]
     public DateTime Modified { get; set; }
 
     [JsonProperty("keywords")]
     public string[]? Keywords { get; set; }
-
-    [JsonProperty("blobName")]
-    public string? BlobName { get; set; }
 }
