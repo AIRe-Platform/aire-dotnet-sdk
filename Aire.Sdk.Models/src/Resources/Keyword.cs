062e8cb4576f61f517af4bcd412081ef78b29e36
Niko Junnila <niko.junnila@goodlife.technology>
2024-05-15 13:47:49 +0300

    Use resource types for stats keys

--- Aire.Sdk.Models/src/Resources/Keyword.cs
+++ Aire.Sdk.Models/src/Resources/Keyword.cs
@@ -4,8 +4,6 @@ namespace Aire.Sdk.Models.Resources;
 
 public class KeywordStats : Dictionary<string, dynamic>
 {
-    public const string ContentCount = "content_count";
-    public const string QuestionnaireCount = "questionnaire_count";
 }
 
 public class Keyword
