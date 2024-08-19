843d362292e2b4976b6b0ab923e173ecf557d351
JuanDGoodlifeTechnology <juan.deltell@goodlife.technology>
2024-08-19 10:42:17 +0300

    Adapting to MPL 2.0 license.

--- Aire.Sdk.AspNetCore/src/HttpRequestExtensions.cs
+++ Aire.Sdk.AspNetCore/src/HttpRequestExtensions.cs
@@ -1,4 +1,9 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
+
+using Microsoft.AspNetCore.Http;
 using Aire.Sdk.Helpers;
 
 namespace Aire.Sdk.AspNetCore
