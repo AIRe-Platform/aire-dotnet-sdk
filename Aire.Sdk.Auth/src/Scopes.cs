ce1fbba125381e22ff6c3682ab70e50dc154719e
Niko Junnila <niko.junnila@goodlife.technology>
2026-09-15 15:17:11 +0300

    Ensure build-in scopes immutability

--- Aire.Sdk.Auth/src/Scopes.cs
+++ Aire.Sdk.Auth/src/Scopes.cs
@@ -3,7 +3,18 @@
 // file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
 
+using System.Collections.ObjectModel;
+
 namespace Aire.Sdk.Auth;
+
+public class AireReadOnlyScopes(params dynamic[] collection)
+    : ReadOnlyCollection<string>(new AireScopes(collection))
+{
+    public AireScopes GetMutableCopy()
+    {
+        return new AireScopes(this);
+    }
+}
 
 public class AireScopes : List<string>
 {
@@ -11,71 +22,71 @@ public class AireScopes : List<string>
     public const string EditProfile = "profile-edit";
     public const string DeleteProfile = "profile-delete";
     public const string ConnectProfile = "profile-connect";
-    public static readonly AireScopes Profile = [ReadProfile, EditProfile, DeleteProfile, ConnectProfile];
+    public static readonly AireReadOnlyScopes Profile = new(ReadProfile, EditProfile, DeleteProfile, ConnectProfile);
 
     public const string ReadChatHistory = "chat-history-read";
     public const string WriteChatHistory = "chat-history-write";
     public const string DeleteChatHistory = "chat-history-delete";
-    public static readonly AireScopes ChatHistory = [ReadChatHistory, WriteChatHistory, DeleteChatHistory];
+    public static readonly AireReadOnlyScopes ChatHistory = new(ReadChatHistory, WriteChatHistory, DeleteChatHistory);
 
     public const string ReadQuestionnaire = "questionnaire-read";
     public const string WriteQuestionnaire = "questionnaire-write";
     public const string DeleteQuestionnaire = "questionnaire-delete";
-    public static readonly AireScopes Questionnaire = [ReadQuestionnaire, WriteQuestionnaire, DeleteQuestionnaire];
+    public static readonly AireReadOnlyScopes Questionnaire = new(ReadQuestionnaire, WriteQuestionnaire, DeleteQuestionnaire);
 
     public const string ReadPublicQuestionnaireResults = "pub-q-results-read";
     public const string DeletePublicQuestionnaireResults = "pub-q-results-delete";
-    public static readonly AireScopes PublicQuestionnaireResults = [ReadPublicQuestionnaireResults, DeletePublicQuestionnaireResults];
+    public static readonly AireReadOnlyScopes PublicQuestionnaireResults = new(ReadPublicQuestionnaireResults, DeletePublicQuestionnaireResults);
 
     public const string ReadContent = "content-read";
     public const string WriteContent = "content-write";
     public const string DeleteContent = "content-delete";
     public const string RateContent = "content-rate";
-    public static readonly AireScopes Content = [ReadContent, WriteContent, DeleteContent, RateContent];
+    public static readonly AireReadOnlyScopes Content = new(ReadContent, WriteContent, DeleteContent, RateContent);
 
     public const string ReadReminders = "reminder-read";
     public const string WriteReminders = "reminder-write";
     public const string DeleteReminders = "reminder-delete";
-    public static readonly AireScopes Reminders = [ReadReminders, WriteReminders, DeleteReminders];
+    public static readonly AireReadOnlyScopes Reminders = new(ReadReminders, WriteReminders, DeleteReminders);
 
     public const string ReadDocument = "document-read";
     public const string WriteDocument = "document-write";
     public const string DeleteDocument = "document-delete";
-    public static readonly AireScopes Document = [ReadDocument, WriteDocument, DeleteDocument];
+    public static readonly AireReadOnlyScopes Document = new(ReadDocument, WriteDocument, DeleteDocument);
 
     public const string ChatCompletion = "chat-completion";
     public const string ChatSummary = "chat-summary";
     public const string ChatTokenCount = "chat-token-count";
-    public static readonly AireScopes ChatBot = [ChatCompletion, ChatSummary, ChatTokenCount];
+    public static readonly AireReadOnlyScopes ChatBot = new(ChatCompletion, ChatSummary, ChatTokenCount);
 
     public const string ReadDemoGroups = "demo-group-read";
     public const string EditDemoGroups = "demo-group-write";
     public const string DeleteDemoGroups = "demo-group-delete";
-    public static readonly AireScopes DemoGroups = [ReadDemoGroups, EditDemoGroups, DeleteDemoGroups];
+    public static readonly AireReadOnlyScopes DemoGroups = new(ReadDemoGroups, EditDemoGroups, DeleteDemoGroups);
 
     public const string ReadServices = "services-read";
     public const string EditServices = "services-edit";
     public const string DeleteServices = "services-delete";
-    public static readonly AireScopes Services = [ReadServices, EditServices, DeleteServices];
+    public static readonly AireReadOnlyScopes Services = new(ReadServices, EditServices, DeleteServices);
 
     public const string ReadKeywords = "keywords-read";
     public const string WriteKeywords = "keywords-write";
     public const string DeleteKeywords = "keywords-delete";
-    public static readonly AireScopes Keywords = [ReadKeywords, WriteKeywords, DeleteKeywords];
+    public static readonly AireReadOnlyScopes Keywords = new(ReadKeywords, WriteKeywords, DeleteKeywords);
 
     public const string ReadStatistics = "statistics-read";
     public const string WriteStatistics = "statistics-write";
-    public static readonly AireScopes Statistics = [ReadStatistics, WriteStatistics];
+    public static readonly AireReadOnlyScopes Statistics = new(ReadStatistics, WriteStatistics);
 
     public const string ReadClients = "clients-read";
     public const string CreateClients = "clients-create";
     public const string EditClients = "clients-edit";
     public const string DeleteClients = "clients-delete";
-    public static readonly AireScopes Clients = [ReadClients, CreateClients, EditClients, DeleteClients];
+    public static readonly AireReadOnlyScopes Clients = new(ReadClients, CreateClients, EditClients, DeleteClients);
 
     public const string ReadAccounts = "accounts-read";
     public const string EditAccounts = "accounts-edit";
-    public static readonly AireScopes Accounts = [ReadAccounts, EditAccounts];
+    public static readonly AireReadOnlyScopes Accounts = new(ReadAccounts, EditAccounts);
 
     public const string AdminConfig = "admin-config";
     public const string AdminAgents = "admin-agents";
@@ -93,7 +104,7 @@ public class AireScopes : List<string>
     public const string FeatureTokenCount = "feat-token-count";
     public const string FeatureCustomPrompt = "feat-custom-prompt";
 
-    public static readonly AireScopes AdminScopes = new(
+    public static readonly AireReadOnlyScopes AdminScopes = new(
         Profile,
         ChatHistory,
         ChatBot,
@@ -121,7 +132,7 @@ public class AireScopes : List<string>
         FeatureCustomPrompt
     );
 
-    public static readonly AireScopes UserScopes = new(
+    public static readonly AireReadOnlyScopes UserScopes = new(
         Profile,
         ChatHistory,
         ChatBot,
@@ -136,7 +147,7 @@ public class AireScopes : List<string>
         ListPlatforms
     );
 
-    public static readonly AireScopes DemoAdminScopes = new(
+    public static readonly AireReadOnlyScopes DemoAdminScopes = new(
         UserScopes,
         DemoGroups,
         FeatureTokenCount,
@@ -145,7 +156,7 @@ public class AireScopes : List<string>
         ListPlatforms
     );
 
-    public static readonly AireScopes DemoUserScopes = new(
+    public static readonly AireReadOnlyScopes DemoUserScopes = new(
         ReadProfile,
         EditProfile,
         ChatBot,
@@ -160,7 +171,7 @@ public class AireScopes : List<string>
         ListPlatforms
     );
 
-    public static readonly AireScopes KeyUserScopes = new(
+    public static readonly AireReadOnlyScopes KeyUserScopes = new(
         Profile,
         ChatHistory,
         ChatBot,
@@ -185,7 +196,7 @@ public class AireScopes : List<string>
         FeatureCustomPrompt
     );
 
-    public static readonly AireScopes TrialUserScopes = new(
+    public static readonly AireReadOnlyScopes TrialUserScopes = new(
         ChatBot,
         ReadChatHistory,
         WriteChatHistory,
@@ -199,8 +210,8 @@ public class AireScopes : List<string>
         TrialAccountUpgrade
     );
 
-    public static readonly Dictionary<string, AireScopes> DefaultRoleScopes = new() {
-        { AireRoles.NonMember, [ListPlatforms]},
+    public static readonly Dictionary<string, AireReadOnlyScopes> DefaultRoleScopes = new() {
+        { AireRoles.NonMember, new(ListPlatforms)},
         { AireRoles.User, UserScopes },
         { AireRoles.Admin, AdminScopes },
         { AireRoles.DemoAdmin, DemoAdminScopes },
@@ -209,7 +220,7 @@ public class AireScopes : List<string>
         { AireRoles.TrialUser, TrialUserScopes },
     };
 
-    public static readonly Dictionary<string, AireScopes> ScopeAliasDict = new() {
+    public static readonly Dictionary<string, AireReadOnlyScopes> ScopeAliasDict = new() {
         { "admin", AdminScopes },
         { "chat", ChatBot },
         { "chat-history", ChatHistory },
@@ -235,7 +246,7 @@ public class AireScopes : List<string>
         var arr = scopes.Split(" ", StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries);
         foreach (var scope in arr)
         {
-            if (ScopeAliasDict != null && ScopeAliasDict.TryGetValue(scope, out AireScopes? value))
+            if (ScopeAliasDict != null && ScopeAliasDict.TryGetValue(scope, out var value))
                 result.AddRange(value);
             else
                 result.Add(scope);
